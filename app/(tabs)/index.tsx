14303833d19f009c5590885bcba0b75bcd733bac
mattiolka <mattiolka@gmail.com>
2025-10-07 06:53:20 +0300

    Add buttons

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,7 @@
 import { View, StyleSheet } from 'react-native';
 
 import Button from '@/components/Button';
-import ImageViewer from "@/components/ImageViewer";
+import ImageViewer from '@/components/ImageViewer';
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
@@ -12,7 +12,7 @@ export default function Index() {
                 <ImageViewer imgSource={PlaceholderImage} />
             </View>
             <View style={styles.footerContainer}>
-                <Button label="Choose a photo" />
+                <Button theme="primary" label="Choose a photo" />
                 <Button label="Use this photo" />
             </View>
         </View>
