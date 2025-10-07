19bfc71ee00bc8b65d2ca68201dc17bd56b699bd
Oksana <mattiolka@gmail.com>
2025-10-07 15:15:23 +0300

    Select image

--- components/ImageViewer.tsx
+++ components/ImageViewer.tsx
@@ -1,13 +1,16 @@
 import { ImageSourcePropType, StyleSheet } from 'react-native';
-import { Image } from "expo-image";
+import { Image } from 'expo-image';
 
 type Props = {
     imgSource: ImageSourcePropType;
+    selectedImage?: string;
 };
 
-export default function ImageViewer({ imgSource }: Props){
-    return <Image source={imgSource} style={styles.image} />
-};
+export default function ImageViewer({ imgSource, selectedImage }: Props) {
+    const imageSource = selectedImage ? { uri: selectedImage } : imgSource;
+
+    return <Image source={imageSource} style={styles.image} />;
+}
 
 const styles = StyleSheet.create({
     image: {
